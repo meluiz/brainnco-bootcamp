d716a9ab45255cb141b9df01fc86f0c529360951
Luiz Felipe <52682525+meluiz@users.noreply.github.com>
2021-09-01 22:47:58 -0300

    Importando os componentes

--- 01_desafio/src/App.js
+++ 01_desafio/src/App.js
@@ -1,3 +1,9 @@
+import Header from './components/header'
+import Menu from './components/menu'
+import Sidebar from './components/sidebar'
+import Main from './components/main'
+import Footer from './components/footer'
+
 const App = () => {
   return (
     <h1>App</h1>
