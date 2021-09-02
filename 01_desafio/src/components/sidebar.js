f2dfcfa666b5239e688f7e9cc2e86038aa27a596
Luiz Felipe <52682525+meluiz@users.noreply.github.com>
2021-09-02 01:06:54 -0300

    Estruturando o componente sidebar

--- 01_desafio/src/components/sidebar.js
+++ 01_desafio/src/components/sidebar.js
@@ -1,8 +1,30 @@
-const Sidebar = ({ children }) => {
+import { H4, H5 } from './heading'
+import Button from './button'
+import Image from './image'
+import Text from './paragraph'
+
+const Sidebar = () => {
   return (
     <aside className="sidebar">
       <div className="box" data-label="Sidebar">
-        { children }
+        <div className="sidebar-container">
+          <div className="sidebar-sticky">
+            <div className="sidebar-info">
+              <H4>Criatividade</H4>
+              <Text small>Esse conteúdo mostra o poder do React.</Text>
+              <Button kind="secondary">
+                Feedback
+              </Button>
+            </div>
+            <div className="sidebar-user">
+              <Image src="/meluiz-avatar.jpg" alt="Avatar luiz" />
+              <div className="sidebar-user__info">
+                <H5>Luiz Felipe</H5>
+                <Text small>Desenvolvedor Web</Text>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </aside>
   )
