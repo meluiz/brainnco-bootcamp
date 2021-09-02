e9d1bd2e2aec9c95e4c3a16c6fb80883e4ec69ce
Luiz Felipe <52682525+meluiz@users.noreply.github.com>
2021-09-02 01:39:01 -0300

    Estrutura do main

--- 01_desafio/src/components/main.js
+++ 01_desafio/src/components/main.js
@@ -1,8 +1,40 @@
-const Main = ({ children }) => {
+import { H1, H2 } from './heading'
+import Text from './paragraph'
+
+const Main = () => {
   return (
     <main className="main">
       <div className="box" data-label="Main">
-        { children }
+       <div className="main-container">
+          <H1>Frases de Lerolero</H1>
+          <Text>Caros amigos, o desafiador cenário globalizado possibilita uma
+           melhor visão global do processo de comunicação como um todo.</Text>
+          <Text>Neste sentido, o desafiador cenário globalizado obstaculiza a
+          apreciação da importância dos procedimentos normalmente adotados.</Text>
+          <Text>Pensando mais a longo prazo, o consenso sobre a necessidade de
+           qualificação apresenta tendências no sentido de aprovar a manutenção
+           das direções preferenciais no sentido do progresso.</Text>
+
+          <H2>E porque não Mussum Ipsum?</H2>
+          <Text>Mussum Ipsum, cacilds vidis litro abertis. Todo mundo vê os
+            porris que eu tomo, mas ninguém vê os tombis que eu levo! Leite de
+            capivaris, leite de mula manquis sem cabeça. Paisis, filhis,
+            espiritis santis. Interessantiss quisso pudia ce receita de bolis,
+            mais bolis eu num gostis.</Text>
+          <Text>Mussum Ipsum, cacilds vidis litro abertis. Mais vale um bebadis
+            conhecidiss, que um alcoolatra anonimis. Quem num gosta di mé, boa
+            gentis num é. Admodum accumsan disputationi eu sit. Vide electram
+            sadipscing et per. A ordem dos tratores não altera o pão duris.</Text>
+
+          <H2>Não se esqueça do Kitty Ipsum...</H2>
+          <Text>Miyāʾūṉ mjau miyāʾūṉ nyā niaou nyā miav meong miyav muning.
+            Miaou ya-ong miav mňau. Myām̥ō meo njäu meogre nyav. Meogre niaou
+            miáú mňau myām̥ō njäu muning mjau. Ngiyaw mi'au njäu miav mjau. Mjau
+            meong nyav miáú mňau meo niaou. Miáú meong meow mjau ya-ong. Miyāʾūn
+            mao miau meo myau ngeung mi'au mňau miaŭ. Mjau mjau miaŭ myām̥ō.</Text>
+
+          <H2>I-Isso é tudo, p-p-pessoal</H2>
+       </div>
       </div>
     </main>
   )
