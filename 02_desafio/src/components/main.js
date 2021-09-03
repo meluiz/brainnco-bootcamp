b5bb737b71d8cc4b3ee550edbb72200f19f1dc1e
Luiz Felipe <52682525+meluiz@users.noreply.github.com>
2021-09-03 15:04:43 -0300

    Estruturando a main

--- 02_desafio/src/components/main.js
+++ 02_desafio/src/components/main.js
@@ -1,7 +1,21 @@
-const Main = () => {
+import { H1 } from './heading'
+
+const Main = ({ icon, title, content }) => {
   return (
     <main className="main">
-      Main
+      <div className="container">
+        <article className="main-content">
+          <div className="main-header">
+            <div className="main-icon">
+              { icon }
+            </div>
+            <H1>{ title }</H1>
+          </div>
+          <div>
+            { content }
+          </div>
+        </article>
+      </div>
     </main>
   )
 }
