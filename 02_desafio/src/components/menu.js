e29f47333bb8b1889ee5e98b8be8cc5d42be058a
Luiz Felipe <52682525+meluiz@users.noreply.github.com>
2021-09-03 13:41:41 -0300

    Estruturando e estilizando o menu

--- 02_desafio/src/components/menu.js
+++ 02_desafio/src/components/menu.js
@@ -1,9 +1,18 @@
+import Button from "./button"
+
 const Menu = () => {
   return (
     <nav className="menu">
       <div className="container">
         <div className="menu-content">
-          <span className="icon">📕</span>
+          <div className="menu-buttons">
+            <Button>
+              Change cover
+            </Button>
+            <Button>
+              Reposition
+            </Button>
+          </div>
         </div>
       </div>
     </nav>
