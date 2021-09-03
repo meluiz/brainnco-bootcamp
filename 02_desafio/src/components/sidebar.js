34a0640c9b08fab9483be4ef57ddf0b2dbbdba86
Luiz Felipe <52682525+meluiz@users.noreply.github.com>
2021-09-03 14:41:44 -0300

    Listando e criando a função do onClick

--- 02_desafio/src/components/sidebar.js
+++ 02_desafio/src/components/sidebar.js
@@ -1,7 +1,24 @@
-const Sidebar = () => {
+const Sidebar = ({ articles, handleArticle }) => {
   return (
     <aside className="sidebar">
-      Sidebar
+      <div className="sidebar-content">
+        <ul className="sidebar-list">
+          { articles && articles.map((article) => (
+            <li key={article._id}>
+              <a
+                href="/"
+                onClick={(event) => {
+                  event.preventDefault()
+                  handleArticle(article.icon, article.title, article.content)
+                }}
+              >
+                <span className="icon">{article.icon}</span>
+                {article.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
     </aside>
   )
 }
