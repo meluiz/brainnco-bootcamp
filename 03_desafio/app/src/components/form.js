eeb15d7beaa6ea0556011385f06d6922b8e2d3ad
Luiz Felipe <52682525+meluiz@users.noreply.github.com>
2021-09-07 19:17:58 -0300

    Re-estruturando formulário

--- 03_desafio/app/src/components/form.js
+++ 03_desafio/app/src/components/form.js
@@ -1,82 +1,39 @@
 import React from 'react'
 
 const Form = () => {
-  const [ inputColor, setInputColor ] = React.useState('#ffffff')
-  const [ inputPlate, setInputPlate ] = React.useState('')
-  const [ inputYear, setInputYear ] = React.useState('')
-
-  const handleInputPlate = (event) => {
-    const format = event.target.value.replace(/([a-zA-Z]{3})(\d{4})/, '$1-$2')
-    setInputPlate(format.toUpperCase())
-  }
-
-  const handleInputYear = (event) => {
-    const format = event.target.value.replace(/\D/g, '')
-    setInputYear(format)
-  }
-
   return (
     <div className="form-wrapper">
       <form className="form">
         <div className="form-group">
-          <label className="form-label" htmlFor="image">
-            <span className="label">Link da imagem</span>
-            <input className="input" type="url" name="image" id="image" required />
+          <label className="label" htmlFor="image">
+            <span className="text">Url da imagem</span>
+            <input type="text" className="inputbox" name="image" id="image" />
           </label>
         </div>
         <div className="form-group">
-          <label className="form-label" htmlFor="brandModel">
-            <span className="label">Marca/Modelo</span>
-            <input className="input" type="text" name="brandModel" id="brandModel" required />
+          <label className="label" htmlFor="brandModel">
+            <span className="text">Modelo do carro</span>
+            <input type="text" className="inputbox" name="brandModel" id="brandModel" />
           </label>
         </div>
         <div className="form-group">
-          <label className="form-label" htmlFor="year">
-            <span className="label">Ano de compra</span>
-            <input
-              className="input"
-              type="text"
-              name="year"
-              id="year"
-              maxLength={4}
-              value={inputYear}
-              onChange={handleInputYear}
-              required
-              />
+          <label className="label" htmlFor="year">
+            <span className="text">Ano de fabricação</span>
+            <input type="text" className="inputbox" namee="year" id="year" />
           </label>
         </div>
         <div className="form-group">
-          <label className="form-label" htmlFor="plate">
-            <span className="label">Placa</span>
-            <input
-              className="input"
-              type="text"
-              name="plate"
-              id="plate"
-              value={inputPlate}
-              onChange={handleInputPlate}
-              maxLength={8}
-              required
-            />
+          <label className="label" htmlFor="color">
+            <span className="text">Cor do carro</span>
+            <div className="form-subgroup">
+              <input type="color" className="inputcolor" id="color" />
+              <input type="text" className="inputbox" name="color" />
+            </div>
           </label>
         </div>
         <div className="form-group">
-          <label className="form-label" htmlFor="color">
-            <span className="label">Color</span>
-            <input
-              type="color"
-              className="input-color"
-              id="color"
-              value={inputColor}
-              onChange={(event) => setInputColor(event.target.value)}
-              required
-              />
-            <input className="input" type="text" name="color" value={inputColor} readOnly />
-          </label>
-        </div>
-        <div className="form-group">
-          <button className="form-submit" type="submit">
-            Salvar e continuar
+          <button className="form-submit" type="button">
+            Salvar
           </button>
         </div>
       </form>
