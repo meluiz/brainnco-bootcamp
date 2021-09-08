359be60e5dc1776ed70863e883e62b0423ee1371
Luiz Felipe <52682525+meluiz@users.noreply.github.com>
2021-09-08 10:48:12 -0300

    Estado para constrole dos carros

--- 03_desafio/app/src/App.js
+++ 03_desafio/app/src/App.js
@@ -1,13 +1,17 @@
+import React from "react"
 import Form from "./components/form"
 import Table from "./components/table"
 
 const App = () => {
+
+  const [ cars, setCars ] = React.useState([])
+
   return (
     <div className="wrapper">
       <div className="container">
         <div className="flexible">
-          <Form />
-          <Table />
+          <Form cars={cars} setCars={setCars} />
+          <Table cars={cars} setCars={setCars} />
         </div>
       </div>
     </div>
