3c953bcdfe4409d37e9e24dbe1f6707af8101779
Luiz Felipe <52682525+meluiz@users.noreply.github.com>
2021-09-10 14:31:23 -0300

    Estilo global

--- 04_desafio/01_desafio/src/index.js
+++ 04_desafio/01_desafio/src/index.js
@@ -1,9 +1,14 @@
 import ReactDOM from 'react-dom'
 import App from './App'
 
+import { GlobalStyle } from './theme/globals'
+
 import './theme/styles.css'
 
 ReactDOM.render(
-  <App />,
+  <>
+    <GlobalStyle />
+    <App />
+  </>,
   document.querySelector('#root')
 )
